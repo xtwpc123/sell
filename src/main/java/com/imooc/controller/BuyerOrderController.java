964b01fddd348d773619ca5a5ff1d150ef73a66a
rocketpc <Tju_pc@163.com>
2019-06-21 23:30:19 +0800

    第一次提交”

--- src/main/java/com/imooc/controller/BuyerOrderController.java
+++ src/main/java/com/imooc/controller/BuyerOrderController.java
@@ -89,12 +89,11 @@ public class BuyerOrderController {
     }
 
     // 取消订单
+
     @PostMapping("/cancel")
     public ResultVO cancel(@RequestParam("openid") String openid,
                            @RequestParam("orderId") String orderId) {
-
-
-            buyerService.cancelOrder(openid, orderId);
+        buyerService.cancelOrder(openid, orderId);
         return ResultVOUtil.success();
     }
 }
