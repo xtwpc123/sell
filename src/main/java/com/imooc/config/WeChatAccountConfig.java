34fc3f88875e19692f98a9d6414ed61a756bdeb7
rocketpc <Tju_pc@163.com>
2019-07-21 20:29:12 +0800

    微信支付，使用pay-sdk

--- src/main/java/com/imooc/config/WeChatAccountConfig.java
+++ src/main/java/com/imooc/config/WeChatAccountConfig.java
@@ -9,10 +9,32 @@ import org.springframework.stereotype.Component;
  * @date: 2019/6/19 20:57
  * @description:
  */
-//@Data
-//@Component
-//@ConfigurationProperties(prefix = "wechat")
-//public class WeChatAccountConfig {
-//
-//    private String
-//}
+@Data
+@Component
+@ConfigurationProperties(prefix = "wechat")
+public class WeChatAccountConfig {
+
+    private String mpAppId;
+
+    private String mpAppSecret;
+
+    /**
+     * 商户号
+     */
+    private String mchId;
+
+    /**
+     * 商户密钥
+     */
+    private String mchKey;
+
+    /**
+     * 商户证书路径
+     */
+    private String keyPath;
+
+    /**
+     * 微信支付异步通知
+     */
+    private String notifyUrl;
+}
